16ea77504f7b61288af9e4c25c47dc98bb6cfca6
지민 백 <qorwlalsjimin@naver.com>
2022-03-25 11:15:20 +0900

    1부터 세서 위치 알려주기

--- CT/getMin.c
+++ CT/getMin.c
@@ -2,15 +2,15 @@
 int main() {
 	int arr[10] = { 2,5,78,43,-45,68,31,100,45,23 };
 	int min = arr[0];
-	int index = 0;
+	int minPosition = 0;
 
 	for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
 		if (min > arr[i]) {
 			min = arr[i];
-			index = i; 
+			minPosition = i; 
 		}
 	}
 
-	printf("최소값 : %d\n최소값의 위치 : arr[%d]", min, index);
+	printf("최소값 : %d\n최소값의 위치 : %d", min, ++minPosition);
 	return 0;
 }
