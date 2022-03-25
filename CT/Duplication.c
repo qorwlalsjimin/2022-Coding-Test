a8c13228f32923444e845b0576e4fe42b15208f1
지민 백 <qorwlalsjimin@naver.com>
2022-03-25 12:12:13 +0900

    b=a가 안 되는 이유

--- CT/Duplication.c
+++ CT/Duplication.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 int main() {
 	int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
 	int b[10];
@@ -6,6 +6,9 @@ int main() {
 
 	for (i = 0; i < 10; i++)
 		b[i] = a[i];
+	//b=a 가 안 됨!
+	//b가 뜻하는 것은 주소. a가 뜻하는 것은 주소
+	//주소에 주소를 넣는다? 이상해!
 
 	for (i = 0; i < 10; i++)
 		printf("%d ", b[i]);
