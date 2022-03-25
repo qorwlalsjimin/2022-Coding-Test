7e4e71ed5c2c61005b1697888d6d88d350d436a0
지민 백 <qorwlalsjimin@naver.com>
2022-03-25 12:04:58 +0900

    예외 처리

--- CT/RandomSearch.c
+++ CT/RandomSearch.c
@@ -6,12 +6,15 @@ int main() {
 	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
 	int n=0, i;
 	srand(time(NULL));
-	n = (rand()%10)+1;
+	n = (rand()%20)+1;
 
 	for (i = 0; i < sizeof(arr) / sizeof(int); i++)
 		if (n == arr[i])
 			break;
 
-	printf("%d는 %d번째 요소에 있습니다.", n, ++i);
+	if (n > 0 && n < 11)
+		printf("%d는 %d번째 요소에 있습니다.", n, ++i);
+	else
+		printf("%d 결과를 찾을 수 없습니다.",n);
 	return 0;
 }
