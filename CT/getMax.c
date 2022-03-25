e4b04fe8985c807b623efb665f5490e40a42c525
지민 백 <qorwlalsjimin@naver.com>
2022-03-25 11:13:49 +0900

    index말고 1부터 세서 위치 표시

--- CT/getMax.c
+++ CT/getMax.c
@@ -2,15 +2,15 @@
 int main() {
 	int arr[10] = { 2,5,78,43,-45,68,31,100,45,23 };
 	int max = 0;
-	int index = 0;
+	int maxPosition = 0;
 
 	for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
 		if (max < arr[i]) {
 			max = arr[i];
-			index = i;
+			maxPosition = i;
 		}
 	}
 
-	printf("최대값 : %d\n최대값의 위치 : arr[%d]", max, index);
+	printf("최대값 : %d\n최대값의 위치 : %d", max, ++maxPosition);
 	return 0;
 }
