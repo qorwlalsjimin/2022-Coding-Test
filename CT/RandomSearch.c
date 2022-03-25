62a7fbc8f62893fe99edfb5097940385d810edc4
지민 백 <qorwlalsjimin@naver.com>
2022-03-25 12:02:48 +0900

    random 제대로 돌리기

--- CT/RandomSearch.c
+++ CT/RandomSearch.c
@@ -1,14 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int n=0, nPosition=0;
-	n = rand()%10+1;
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int n=0, i;
+	srand(time(NULL));
+	n = (rand()%10)+1;
 
-	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
-		if (arr[i] == n) nPosition = i;
+	for (i = 0; i < sizeof(arr) / sizeof(int); i++)
+		if (n == arr[i])
+			break;
 
-	printf("%d는 %d번째 요소에 있습니다.", n, nPosition);
+	printf("%d는 %d번째 요소에 있습니다.", n, ++i);
 	return 0;
 }
--- CT/Search.c
+++ CT/Search.c
@@ -1,15 +1,15 @@
 ﻿#include <stdio.h>
 int main() {
 	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int n, nPosition;
+	int n, i;
 	do {
 		printf("1~10 중 하나를 입력하시오. >> ");
 		scanf_s("%d", &n);
 	} while (n < 1 || n>10);
 
-	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
-		if (arr[i] == n) nPosition = i;
+	for (i = 0; i < sizeof(arr) / sizeof(int); i++)
+		if (arr[i] == n) break;
 
-	printf("%d는 %d번째 요소에 있습니다.", n, nPosition);
+	printf("%d는 %d번째 요소에 있습니다.", n, ++i);
 	return 0;
 }
